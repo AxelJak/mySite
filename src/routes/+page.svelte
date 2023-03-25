7974df18505a7d9311100ade3b128099f53f0463
Axel Jakobsson <c18ajn@cs.umu.se>
2023-03-25 11:33:17 +0100

    Add pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,112 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    <nav class="grid">
+        <a class="home" href="/">home</a>
+        <a class="info" href="/info">info</a>
+    </nav>
+    
+    <div class="grid container">
+        <h4 class="intro" >hello my name is <br> &nbsp;&nbsp;&nbsp;&nbsp; Axel Jakobsson</h4>
+        <h4 class="web-developer" >i'm a web developer</h4>
+        <img class="norge" src="norge.jpg" alt="Axel in Norway">
+        <img class="blabar" src="blabar.jpg" alt="Axel eating blueberries">
+        <h4 class="umea">based in umeå</h4>
+        <a class="my_work" href="my_work.hmtl">my work</a>
+        
+    </div>
+    <footer class="grid">
+        <h5 class="contact">contact - <br> axel.jakobsson3@gmail.com</h5>
+    </footer>
+</body>
+
+<style>
+    * {
+    background-color: white;
+    font-family: monospace, sans-serif;
+    }
+
+    a {
+        color: black;
+        text-decoration:underline;
+        font-size: 1.2rem;
+    }
+    a:hover { 
+        color: rgb(210, 204, 204);
+    }
+
+    a:visited {
+        text-decoration: none;
+    }
+
+    nav a {
+        font-size: 3rem;
+    }
+    nav {
+        grid-template-columns: 7fr 1fr 1fr 1fr;
+        justify-items: center;
+        grid-template-areas:
+        ". home info .";
+        margin: 3rem 0px;
+    }
+
+    .home {
+        grid-area: home;
+    }
+    .info {
+        grid-area: info;
+    }
+
+    .grid {
+        display: grid;
+    }
+
+    .container {
+        grid-template-columns: auto auto;
+        place-items: center;
+        grid-template-areas: 
+        "intro bar"
+        "web bar"
+        "norge bar" 
+
+        "norge work"
+        "norge umea";
+
+    }
+
+    h4 {
+        font-size: 1.2rem;
+        font-weight: 300;
+    }
+
+    .blabar {
+        grid-area: bar;
+    }
+
+    .norge {
+        grid-area: norge;
+    }
+
+    .intro {
+        grid-area: intro;
+    }
+
+    .web-developer {
+        grid-area: web;
+    }
+
+    .my_work {
+        grid-area: work;
+    }
+
+    .umea {
+        grid-area: umea;
+    }
+
+    img {
+        padding: 2rem;
+        width: 30rem;
+    }
+
+    footer {
+        justify-items: center;
+    }
+</style>
---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,91 @@
+<body>
+    <nav class="grid">
+        <a class="home" href="/">home</a>
+        <a class="info" href="/info">info</a>
+    </nav>
+    <div class="box flex">
+        
+        <div class="work flex">
+            <h1>XLENT</h1>
+            <p>Right now I work at XLENT as a web developer.</p>
+        </div>
+
+        <div class="work flex">
+            <h1>BAE Systems Hägglunds</h1>
+            <p>I worked two summers at BAE Systems Hägglunds as a system developer. Developing tools 
+                that made the internal processes more efficient. We used python to build applications.
+            </p>
+        </div>
+
+        <div class="work flex">
+            <h1>Övik lunch redesign</h1>
+            <h2>My design: <a href="https://wondrous-parfait-4e37b1.netlify.app/">My version</a></h2>
+            <h2>Original design: <a href="https://lunch.ovik.info/">Original Site</a></h2>
+            <p>
+                Övik has a lunch resturantwebiste that keeps track of the lunch menus in the city. I didn't like the design of the website so I decided to redesign it. 
+            </p>
+        </div>
+        
+    </div>
+    
+</body>
+
+<style>
+    * {
+    background-color: white;
+    font-family: monospace, sans-serif;
+    }
+
+    a {
+        color: black;
+        text-decoration:underline;
+        font-size: 1.2rem;
+    }
+    a:hover { 
+        color: rgb(210, 204, 204);
+    }
+
+    a:visited {
+        text-decoration: none;
+    }
+
+    nav a {
+        font-size: 3rem;
+    }
+    nav {
+        grid-template-columns: 7fr 1fr 1fr 1fr;
+        justify-items: center;
+        grid-template-areas:
+        ". home info .";
+        margin: 3rem 0px;
+    }
+
+    .home {
+        grid-area: home;
+    }
+    .info {
+        grid-area: info;
+    }
+
+    .grid {
+        display: grid;
+    }
+
+    .flex {
+        display: flex;
+    }
+
+    .box {
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .work {
+        justify-items: center;
+        align-items: center;
+        flex-wrap: wrap;
+        border: 1px solid black;
+        width: 50%;
+    }
+
+</style>
